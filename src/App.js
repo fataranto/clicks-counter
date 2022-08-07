f7688bf665b0126f568c648c3dffda6184d62ba5
fataranto <fataranto@gmail.com>
2022-08-07 22:05:46 +0200

    elements and styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import './App.css';
+import Boton from './components/Button';
+import Contador from './components/Counter';
 import freeCodeCampLogo from './imgs/fcc_primary.svg';
 
 function App() {
+
+  const manejarClick = () => {
+    console.log('Clic');
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
+
   return (
     <div className='App'>
-      <div className='freecodecoamp-logo-contenedor'>
+      <div className='freecodecamp-logo-contenedor'>
         <img
-        className='freecodecoamp-logo'
+        className='freecodecamp-logo'
         src={freeCodeCampLogo}
-        alt='freecodecoamp-logo'
+        alt='logo de FreeCodeCamp'
         />
-        
+      </div>
+      <div className='contenedor-texto'>
+        <Contador numClics='5' />
+      <Boton 
+      texto='Clic'
+      esBotonDeClic={true}
+      manejarClic={manejarClick}/>
+      <Boton 
+      texto='Reiniciar'
+      esBotonDeClic={false}
+      manejarClic={reiniciarContador}/>
 
-    </div>
+      </div>
+
     </div>
   );
 }
