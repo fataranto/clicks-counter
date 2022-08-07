44ae062bbff0dccea43c7e6c510b00c2bdde6810
fataranto <fataranto@gmail.com>
2022-08-07 21:16:08 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import freeCodeCampLogo from './imgs/fcc_primary.svg';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='freecodecoamp-logo-contenedor'>
+        <img
+        className='freecodecoamp-logo'
+        src={freeCodeCampLogo}
+        alt='freecodecoamp-logo'
+        />
+        
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
